50a65135aaec5e00a59913355de73e8c00fdecb9
Pa St <nonamenodejs@gmail.com>
2024-01-07 01:29:07 +0200

    Some changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          Navbar
-        </a>
+        <Link className="navbar-brand" href="/">
+          Work
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
